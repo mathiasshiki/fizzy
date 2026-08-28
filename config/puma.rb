572d392a2ae03194e1be798637dd19918d645e5c
mathiaseklund <mathiaseklund@hotmail.se>
2026-08-28 13:10:42 +0200

    ignore

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,7 @@ unless Rails.env.local?
   # worker per CPU, 1 thread per worker and tune it from there.
   #
   # https://edgeguides.rubyonrails.org/tuning_performance_for_deployment.html#puma
-  workers Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  workers Integer(ENV.fetch("WEB_CONCURRENCY", 0) { Concurrent.physical_processor_count })
   threads 1, 1
 
   # Tell the Ruby VM that we're finished booting up.
